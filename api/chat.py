3e25ce3d84e0f84f206a5f5b15c243818f1db5a7
Pooja Thakur <113713900+Pooja12-cpu@users.noreply.github.com>
2026-02-14 16:53:31 -0600

    Update chat.py

--- api/chat.py
+++ api/chat.py
@@ -1,26 +1,12 @@
 # api/chat.py
 from flask import Flask, render_template, request
-import openai
 import os
 app = Flask(__name__)
-openai.api_key  = os.getenv("OPENAI_API_KEY")#"<place your openai_api_key>"
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
 @app.route("/")
-def home():    
+def index():    
     return render_template("index.html")
-@app.route("/get")
-def get_bot_response():    
-    userText = request.args.get('msg')  
-    response = get_completion(userText)  
-    #return str(bot.get_response(userText)) 
-    return response
+
 if __name__ == "__main__":
+    app.run(debug=False)
 
